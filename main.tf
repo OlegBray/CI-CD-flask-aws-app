046b1a20f41d25094d3ffa0076270fe4a6645b21
‪Oleg Braylovsky‬‏ <92874809+OlegBray@users.noreply.github.com>
2025-05-08 12:13:01 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,9 +30,9 @@ resource "aws_cloudwatch_log_group" "flask_integration_logs_oleg" {
   retention_in_days = 7
 }
 
-# Target Group (shortened name)
-resource "aws_lb_target_group" "flask_integration_target_group" {
-  name        = "flask-tg-oleg"
+# Corrected target group with port 5000
+resource "aws_lb_target_group" "flask_target_group" {
+  name        = "flask-target-group-oleg"
   port        = 5000
   protocol    = "HTTP"
   vpc_id      = data.aws_subnet.subnet_1.vpc_id
@@ -45,7 +45,7 @@ resource "aws_lb_target_group" "flask_integration_target_group" {
   }
 }
 
-# Listener
+# Listener on ALB pointing to target group
 resource "aws_lb_listener" "flask_integration_listener" {
   load_balancer_arn = data.aws_lb.existing_lb.arn
   port              = 5000
@@ -53,12 +53,12 @@ resource "aws_lb_listener" "flask_integration_listener" {
 
   default_action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.flask_integration_target_group.arn
+    target_group_arn = aws_lb_target_group.flask_target_group.arn
   }
 }
 
-# Task Definition
-resource "aws_ecs_task_definition" "flask_integration_task" {
+# Task definition referencing correct image and port
+resource "aws_ecs_task_definition" "flask_task" {
   family                   = "flask-integration-task-oleg-tf"
   network_mode             = "awsvpc"
   requires_compatibilities = ["FARGATE"]
@@ -68,7 +68,7 @@ resource "aws_ecs_task_definition" "flask_integration_task" {
 
   container_definitions = jsonencode([{
     name      = "flask"
-    image     = "314525640319.dkr.ecr.il-central-1.amazonaws.com/imtech-oleg:flask-integration-v1"
+    image     = "314525640319.dkr.ecr.il-central-1.amazonaws.com/imtech-oleg:${BUILD_NUMBER}"
     essential = true
     portMappings = [
       {
@@ -80,19 +80,19 @@ resource "aws_ecs_task_definition" "flask_integration_task" {
     logConfiguration = {
       logDriver = "awslogs"
       options = {
-        "awslogs-group"         = "/ecs/flask-integration-logs-oleg"
-        "awslogs-region"        = "il-central-1"
-        "awslogs-stream-prefix" = "flask"
+        awslogs-group         = "/ecs/flask-integration-logs-oleg"
+        awslogs-region        = "il-central-1"
+        awslogs-stream-prefix = "flask"
       }
     }
   }])
 }
 
-# ECS Service
+# ECS service referencing correct port and target group
 resource "aws_ecs_service" "flask_service" {
   name            = "flask-service-oleg"
   cluster         = data.aws_ecs_cluster.existing_cluster.id
-  task_definition = aws_ecs_task_definition.flask_integration_task.arn
+  task_definition = aws_ecs_task_definition.flask_task.arn
   desired_count   = 1
   launch_type     = "FARGATE"
 
@@ -103,7 +103,7 @@ resource "aws_ecs_service" "flask_service" {
   }
 
   load_balancer {
-    target_group_arn = aws_lb_target_group.flask_integration_target_group.arn
+    target_group_arn = aws_lb_target_group.flask_target_group.arn
     container_name   = "flask"
     container_port   = 5000
   }
